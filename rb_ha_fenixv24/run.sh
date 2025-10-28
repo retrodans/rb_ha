608adb065378aee64a75038db2e92082ffc3daf7
Dan Duke <dan@agile.coop>
2025-10-28 15:04:26 +0000

    Fixing run file to log correct vconfig

--- rb_ha_fenixv24/run.sh
+++ rb_ha_fenixv24/run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
 # LOG: Initialising
-bashio::log.info "Reverse tunnel initializing."
+bashio::log.info "Fenix V24 initialising."
 
 # GET values from config
-username=$(bashio::config 'username')
-host=$(bashio::config 'server.host')
+username=$(bashio::config 'email')
+host=$(bashio::config 'password')
+smarthome_id=$(bashio::config 'smarthome_id')
 
 # GET values from API for temperature
 
 # SET sensors for homeassistant to use
 
 # LOG: Show values
-bashio::log.info "Reverse tunnel configured for $username@$host"
+bashio::log.info "Fenix v24 configured for $email@$smarthome_id"
